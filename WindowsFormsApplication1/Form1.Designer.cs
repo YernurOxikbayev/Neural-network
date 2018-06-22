4f6e941e0cc8238358c2c914f4c96f84fef31c1e
YernurOxikbayev <ernur.oksikbaev@gmail.com>
2018-06-22 11:09:35 +0700

    Добавьте файлы проекта.

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,303 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button4);
+            this.groupBox2.Controls.Add(this.label11);
+            this.groupBox2.Controls.Add(this.label9);
+            this.groupBox2.Controls.Add(this.label8);
+            this.groupBox2.Controls.Add(this.textBox6);
+            this.groupBox2.Controls.Add(this.textBox5);
+            this.groupBox2.Controls.Add(this.textBox4);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.textBox3);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Location = new System.Drawing.Point(13, 136);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(292, 152);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Сложение";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(150, 44);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(47, 23);
+            this.button4.TabIndex = 11;
+            this.button4.Text = "=";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(135, 111);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(0, 13);
+            this.label11.TabIndex = 10;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(11, 124);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(133, 13);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "рассчитанный результат";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(16, 88);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(91, 13);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "точное значение";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(150, 121);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(135, 20);
+            this.textBox6.TabIndex = 6;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(150, 88);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(135, 20);
+            this.textBox5.TabIndex = 5;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(80, 46);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(41, 20);
+            this.textBox4.TabIndex = 3;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(61, 49);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(13, 13);
+            this.label6.TabIndex = 2;
+            this.label6.Text = "+";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(13, 46);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(41, 20);
+            this.textBox3.TabIndex = 1;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(10, 20);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(86, 13);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Сложить числа:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(125, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Скрытых слоев";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(125, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(93, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Нейронов в слое";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(231, 24);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(48, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "1";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(231, 53);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(48, 20);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.Text = "4";
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(14, 19);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(93, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Создать сеть";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(14, 48);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(93, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Обучить сеть";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Visible = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(125, 77);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Ошибка:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(125, 94);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 13);
+            this.label4.TabIndex = 7;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(14, 77);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(93, 23);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Остановить";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Visible = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(13, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(292, 117);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Обучение";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(310, 293);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        const int n = 8, m = 3;
+        int layersNumber, nN = 0;
+        bool stop = false;
+        public double error = 1;
+        static Random rnd = new Random();
+        Layer[] Net;// = new Layer[3];
+
+        double [,]ObuchMas = new double [n,m] {{0.1,0.1,0.2},
+                                               {0.2,0.2,0.4},
+                                               {0.1,0.2,0.3},
+                                               {0.2,0.3,0.5},
+                                               {0.3,0.4,0.7},
+                                               {0.11,0.11,0.22},
+                                               {0.12,0.12,0.24},
+                                               {0.21,0.21,0.42}};
+
+
+        public class Neuron
+        {
+            public double [] w;
+            public double s;
+            public double x;
+            public double b;
+            public int relNumber;
+            public Neuron()
+            {
+            }
+            public void weight()
+            {
+                w = new double[relNumber];
+                for (int i = 0; i < relNumber; i++)
+                    w[i] = rnd.Next(200) / 1000.0 - 0.1;
+                s = 0;
+                x = 0;
+                b = 0;
+                return;
+            }
+        }
+
+        public class Layer
+        {
+            public int neuronsNumber;
+            public Neuron []Neuron;
+            public Layer()
+            {
+                neuronsNumber = 0;
+            }
+            public void createNeurons()
+            { 
+                Neuron = new Neuron[neuronsNumber];
+                for (int i = 0; i < neuronsNumber; i++)
+                {
+                    Neuron[i] = new Neuron();
+                }
+                return;
+            }
+        }
+                
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button2.Visible = true;
+            layersNumber = Convert.ToInt32(textBox1.Text)+2;
+            int neuronsNumber = Convert.ToInt32(textBox2.Text);
+            Net = new Layer[layersNumber];
+
+            for (int i = 0; i < layersNumber; i++)
+            {
+                Net[i] = new Layer();
+            }
+
+            Net[0].neuronsNumber = (m - 1); // input numbers
+            Net[0].createNeurons();
+
+            for (int j = 0; j < Net[0].neuronsNumber; j++)
+                Net[0].Neuron[j].x = ObuchMas[nN,j];
+
+            for (int i = 1; i < layersNumber; i++ )
+            {
+                if (i == (layersNumber - 1)) Net[i].neuronsNumber = 1;
+                else Net[i].neuronsNumber = neuronsNumber;
+                Net[i].createNeurons();
+                for (int j = 0; j < Net[i].neuronsNumber; j++)
+                {
+                    Net[i].Neuron[j].relNumber = Net[i - 1].neuronsNumber; //кол-во связей нейрона=кол-ву нейронов предыдущего слоя
+                    Net[i].Neuron[j].weight();
+                }
+            }
+        }
+
+
+
+        void Result()
+        {
+            double Sum;
+            for (int i = 1; i < layersNumber; i++ )
+            {
+                for (int j = 0; j < Net[i].neuronsNumber; j++)
+                {
+                    Sum = 0;
+                    for (int k = 0; k < Net[i].Neuron[j].relNumber; k++)
+                        Sum += (Net[i - 1].Neuron[k].x * Net[i].Neuron[j].w[k]);
+
+                    Sum += Net[i].Neuron[j].s;
+                    Net[i].Neuron[j].x = 1.0 / (1.0 + Math.Pow(Math.E, -Sum)); ;       //функция активации
+                }
+            }
+        }
+
+
+        void CalcError()
+        {
+            double temp = 0;
+            button3.Visible = true;
+            Net[layersNumber - 1].Neuron[0].b = Net[layersNumber - 1].Neuron[0].x * (1 - Net[layersNumber - 1].Neuron[0].x) * (ObuchMas[nN,m - 1] - Net[layersNumber - 1].Neuron[0].x);
+            //по методу обратного распространения ошибки
+            for (int i = layersNumber - 2; i > 0; i--)
+                for (int j = 0; j < Net[i].neuronsNumber; j++)   //коррекция веса следующего слоя
+                {
+                    temp = 0;
+                    for (int k = 0; k < Net[i + 1].neuronsNumber; k++)
+                        temp += (Net[i + 1].Neuron[k].b * Net[i + 1].Neuron[k].w[j]);
+                    temp *= (Net[i].Neuron[j].x * (1 - Net[i].Neuron[j].x));
+                    Net[i].Neuron[j].b = temp;
+                }
+            for (int i = layersNumber - 2; i >= 0; i--)
+                for (int j = 0; j < Net[i].neuronsNumber; j++)
+                    for (int k = 0; k < Net[i + 1].neuronsNumber; k++)
+                    {
+                        Net[i + 1].Neuron[k].w[j] += (0.8 * Net[i + 1].Neuron[k].b * Net[i].Neuron[j].x);
+                    }
+            for (int i = layersNumber - 1; i > 0; i--)
+                for (int k = 0; k < Net[i].neuronsNumber; k++)
+                    Net[i].Neuron[k].s += (0.8 * Net[i].Neuron[k].b);
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            double temp;
+            stop = false;
+            while (!stop)
+            {
+                error = 0;
+                for (nN = 0; nN < n; nN++)
+                {
+                    for (int j = 0; j < Net[0].neuronsNumber; j++)
+                        Net[0].Neuron[j].x = ObuchMas[nN, j]; //подаем на вход
+                    Result();
+                    CalcError();
+                    temp = ObuchMas[nN, m - 1] - Net[layersNumber - 1].Neuron[0].x;// разн между
+                    if (temp < 0) temp *= -1;
+                    error += temp;
+                }
+                error /= (double)n;
+                label4.Text = Convert.ToString(error);
+                Application.DoEvents();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            stop = true;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            double x1, x2;
+            x1 = Convert.ToDouble(textBox3.Text);
+            x2 = Convert.ToDouble(textBox4.Text);
+
+            textBox5.Text = Convert.ToString(x1+x2);
+            Net[0].Neuron[0].x = x1;
+            Net[0].Neuron[1].x = x2;
+
+            Result();
+            textBox6.Text = Convert.ToString(Net[layersNumber - 1].Neuron[0].x);
+        }
+
+
+    }
+}
